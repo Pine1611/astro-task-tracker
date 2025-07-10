9495d82535d38b8054498fec029257cb212f4cf9
I am Pine <iampine1611@gmail.com>
2025-07-10 08:38:09 +0700

    small change styles

--- src/components/app/Buttons.jsx
+++ src/components/app/Buttons.jsx
@@ -3,7 +3,7 @@ const ButtonAction = ({ handleClick, children, typeBtn }) => {
 		<>
 			<button
 				onClick={handleClick}
-				className={`cursor-pointer items-center justify-center rounded-sm bg-slate-100/80 p-1 transition hover:bg-slate-50 ${typeBtn === "create" && "absolute top-1/2 right-1 flex h-8 w-8 -translate-y-1/2 border border-slate-200"} ${typeBtn === "play" && "border border-blue-50 hover:border-blue-100 hover:bg-blue-50!"}`}>
+				className={`cursor-pointer items-center justify-center rounded-sm bg-slate-100/80 p-1 transition hover:bg-slate-50 ${typeBtn === "create" && "absolute top-1/2 right-1 flex h-8 w-8 -translate-y-1/2 border border-slate-200"} ${typeBtn === "play" && "border border-blue-50 hover:border-blue-100 hover:bg-blue-50!"} ${typeBtn === "done" && "border border-green-50 hover:border-green-100 hover:bg-green-50!"} ${typeBtn === "stop" && "border border-yellow-50 hover:border-yellow-100 hover:bg-yellow-50!"} ${typeBtn === "delete" && "border border-red-50 hover:border-red-100 hover:bg-red-50!"}`}>
 				{children}
 			</button>
 		</>
